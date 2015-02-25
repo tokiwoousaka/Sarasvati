20f1b291614852d800eb3d869dec383bae5ce525
Tokiwo Ousaka <its.out.of.tune.this.my.music@gmail.com>
2015-02-25 21:16:44 +0900

    change cabal file and fix export modules

--- src/Sound/Sarasvati.hs
+++ src/Sound/Sarasvati.hs
@@ -1,10 +1,8 @@
 module Sound.Sarasvati
-  ( module Sound.Sarasvati.Base
-  , module Sound.Sarasvati.Types
-  , module Sound.Sarasvati.List.Stereo 
+  ( module Sound.Sarasvati.Types
+  , module Sound.Sarasvati.List.Stereo
   , Error
   ) where
-import Sound.Sarasvati.Base 
 import Sound.Sarasvati.Types
-import Sound.Sarasvati.List.Stereo 
+import Sound.Sarasvati.List.Stereo
 import Sound.PortAudio
